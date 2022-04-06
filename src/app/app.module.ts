34621e1aec5c8bf187922479e8bd816c16030f67
Rodolphe <rodolphebansard@yahoo.com>
2022-04-06 11:53:37 +0200

    pet care

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LandingPageContentComponent } from './components/landing-page-content/landing-page-content.component';
+import { LandingPageFeaturesComponent } from './components/landing-page-features/landing-page-features.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LandingPageContentComponent,
+    LandingPageFeaturesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/landing-page-features/landing-page-features.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page-features',
+  templateUrl: './landing-page-features.component.html',
+  styleUrls: ['./landing-page-features.component.scss']
+})
+export class LandingPageFeaturesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
